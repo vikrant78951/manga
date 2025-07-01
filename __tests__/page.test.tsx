2a7edc5df7b7961ee6fe3dc0dcffc3cf9e5f1e7c
Vikas Singh <vikrant78951@gmail.com>
2025-07-02 00:44:09 +0530

    Project setup

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "../app/page";
+
+test("Page", () => {
+  render(<Page />);
+  expect(
+    screen.getByRole("heading", {
+      level: 1,
+      name: "Next Js with Shadcn + Typescript + Git Actions + Test",
+    }),
+  ).toBeDefined();
+});
