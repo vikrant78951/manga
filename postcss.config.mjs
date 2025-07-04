a7db5ccc10b1af1bb9020154ca639845f16f8a93
vikrant78951 <vikrant78951@gmail.com>
2025-07-04 23:04:17 +0530

    postcss configured and prepush updated

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
